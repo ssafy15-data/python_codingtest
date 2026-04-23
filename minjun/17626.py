b1c31174668483b125959f5603b4d2eb6c4d067a
MinjunKim <minjun069@naver.com>
2026-04-23 15:25:23 +0900

    누락 파일 추가

--- minjun/17626.py
+++ minjun/17626.py
@@ -1,18 +1,28 @@
+# 특정 수를 제곱수 합으로 표현할 때의 최소 개수 (메모이제이션)
 lag_dict = {num*num:1 for num in range(223, 0, -1)}
+
+# 223의 제곱부터 1의 제곱이 저장된 223개의 리스트
 sq_nums = list(lag_dict.keys())
 
-# num의 제곱수 합의 최소 개수
+# num의 제곱수 합의 최소 개수 (재귀+메모이제이션)
 def lagrangju(n):
+
+    # 이미 저장되어 있다면 return
     if lag_dict.get(n, False):
         return lag_dict[n]
     
+    # 최소 개수 초기화
     answer = 1e9
+    
+    # 223의 제곱부터 1의 제곱까지 순회하며,
+    # n과 같아지거나 더 커지는 순간부터 최소개수 탐색 시작
     for sq_num in sq_nums:
         if n == sq_num:
             return 1
         if n > sq_num:
             answer = min(answer, 1 + lagrangju(n-sq_num))
     
+    # 결과를 저장하고 후에 재사용
     lag_dict[n] = answer
     return answer
 
--- minjun/2169.py
+++ minjun/2169.py
@@ -1,8 +1,5 @@
 import sys
-sys.stdin = open("minjun/sample.txt", 'r')
-
-# import sys
-# input = sys.stdin.readline
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
 matrix = [ list(map(int, input().split())) for _ in range(n) ]
@@ -15,18 +12,24 @@ dp_table[3][0][0] = matrix[0][0]
 for j in range(1, m):
     dp_table[3][0][j] = dp_table[3][0][j-1] + matrix[0][j]
 
+# 1번째 행부터
 for i in range(1, n):
+
+    # 0. 위로부터 이동 (기본값)
     for j in range(m):
         dp_table[0][i][j] = dp_table[3][i-1][j] + matrix[i][j]
 
+    # 1. 왼쪽으로부터 이동 (0.과 비교)
     dp_table[1][i][0] = dp_table[0][i][0]
     for j in range(1, m):
         dp_table[1][i][j] = max(dp_table[0][i][j], dp_table[1][i][j-1] + matrix[i][j])
 
+    # 2. 오른쪽으로부터 이동 (0.과 비교)
     dp_table[2][i][m-1] = dp_table[0][i][m-1]
     for j in range(m-2, -1, -1):
         dp_table[2][i][j] = max(dp_table[0][i][j], dp_table[2][i][j+1] + matrix[i][j])
     
+    # 3. 최대값 (1. 2. 비교)
     for j in range(m):
         dp_table[3][i][j] = max(dp_table[1][i][j], dp_table[2][i][j])
 
