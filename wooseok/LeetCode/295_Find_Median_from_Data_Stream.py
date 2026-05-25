d077ba6ec17e8b9eb207f9398a080cd08f869067
장우석 <us4c0d3@gmail.com>
2026-05-25 19:53:25 +0900

    #137: Find Median from Data Stream
    heap

---
+++ wooseok/LeetCode/295_Find_Median_from_Data_Stream.py
@@ -0,0 +1,25 @@
+import heapq
+
+class MedianFinder:
+
+    def __init__(self):
+        self.left = []  # max_heap
+        self.right = []  # min_heap
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.left, -num)
+        heapq.heappush(self.right, -heapq.heappop(self.left))
+
+        if len(self.left) < len(self.right):
+            heapq.heappush(self.left, -heapq.heappop(self.right))
+
+    def findMedian(self) -> float:
+        if len(self.left) > len(self.right):
+            return float(-self.left[0])
+        return (-self.left[0] + self.right[0]) / 2.0
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
